21b3f434523719cf6e739dfaf271b6f97c5326e0
Quinn Jones <quinnkjones@gmail.com>
2025-09-18 20:11:07 -0400

    setting up the hello world of the django app

---
+++ research_cards_django/my_app.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for research_cards project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+import logging
+logging.info("WSGI file loaded")
+logging.info(os.listdir('.'))
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'research_cards.settings')
+
+application = get_wsgi_application()
---
+++ research_cards_django/rcards_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RcardsAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rcards_app'
